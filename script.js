03cee35427cefcff1e953ee723e827e086ae45fe
Emma Lopez <25lopezev@rbhs208.net>
2025-02-06 23:47:27 -0600

    HTML/CSS/JS update

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed(".auto-type", {
+        strings: ["¡Hola Mundo! I'm Emma."],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    });
+});
