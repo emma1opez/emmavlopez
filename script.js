83c1ab63f7d8e3762bba874f3e6b7f5de8a03bcd
Emma Lopez <25lopezev@rbhs208.net>
2025-02-17 12:46:47 -0600

    proj & recog updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     new Typed(".auto-type", {
-        strings: ["¡Hola Mundo! I'm Emma."],
+        strings: ["¡Hola Mundo! I'm Emma. "],
         typeSpeed: 100,
         backSpeed: 50,
         loop: true
